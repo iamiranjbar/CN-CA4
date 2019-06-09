9975ff7f5b6739a07c2ec44b83fc85dc635ef05a
Ali Edalat <ali.edalat.77@gmail.com>
2019-06-09 12:38:30 -0700

    Fix interface constructor.

--- src/node/Interface.java
+++ src/node/Interface.java
@@ -16,11 +16,14 @@ public class Interface {
 	private String receiverIp;
 	private DatagramSocket datagramSocket;
 	
-	public Interface(int id, String ip, String receiverIp) throws SocketException {
+	public Interface(int id, String ip, String receiverIp, int receiverPort, int port) throws SocketException {
 		this.id = id;
 		this.ip = ip;
 		this.receiverIp = receiverIp;
+		this.receiverPort = receiverPort;
+		this.port = port;
 		datagramSocket = new DatagramSocket(this.port);
+		datagramSocket.setSoTimeout(800);
 	}
 	
 	public int getId() {
@@ -35,13 +38,17 @@ public class Interface {
 		return receiverIp;
 	}
 	
+	public int getReceiverPort() {
+		return receiverPort;
+	}
+	
 	public void send(String packet) throws UnknownHostException, IOException {
 		datagramSocket.send(new DatagramPacket(packet.getBytes(), packet.getBytes().length,
 				InetAddress.getByName(this.receiverIp), this.receiverPort));
 	}
 	
 	public String receive() throws IOException {
-		byte[] data = new byte[64000];
+		byte[] data = new byte[1400];
     	DatagramPacket p = new DatagramPacket(data, data.length);
 		datagramSocket.receive(p);
 		return new String(p.getData());
