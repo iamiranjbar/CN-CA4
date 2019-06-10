2ad288e1851e12a0803f26cb367f3213a24a60ab
amir <iamirranjbar@gmail.com>
2019-06-10 11:16:15 +0430

    Complete lnxParser.

--- src/tools/LnxParser.java
+++ src/tools/LnxParser.java
@@ -1,27 +1,38 @@
 package tools;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class LnxParser {
-    public static void parse(String fileName){
+    public static HostDTO parse(String fileName){
         try {
             BufferedReader reader = new BufferedReader(new FileReader(fileName));
-            String line = reader.readLine();
-            while (line != null){
-                System.out.println(line);
-                String[] parts = line.split("\\s+");
-                line = reader.readLine();
-            }
+            HostDTO node = readOwnData(reader);
+            node.addLinks(readLinks(reader));
             reader.close();
+            return node;
         }catch (IOException e) {
             e.printStackTrace();
+            return null;
         }
     }
 
-    public static void readOwnData(){
+    public static HostDTO readOwnData(BufferedReader reader) throws IOException {
+        String line = reader.readLine();
+        String[] parts = line.split("\\s+");
+        return new HostDTO(parts[0], Integer.parseInt(parts[1]));
+    }
 
+    public static ArrayList<LinkDTO> readLinks(BufferedReader reader) throws IOException {
+        ArrayList<LinkDTO> linkDTOS = new ArrayList<>();
+        String line = reader.readLine();
+        while (line != null){
+            String[] parts = line.split("\\s+");
+            linkDTOS.add(new LinkDTO(parts[0], Integer.parseInt(parts[1]), parts[2], parts[3]));
+            line = reader.readLine();
+        }
+        return linkDTOS;
     }
 }
